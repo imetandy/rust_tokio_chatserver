c9b2718bba9a5cb42fbcfc1b745716fcfcd0d779
imetandy <robinson.andrewjames@gmail.com>
2024-07-29 23:32:32 +0100

    Changed from echo server to chat app via telnet clients.

--- chatserver/src/main.rs
+++ chatserver/src/main.rs
@@ -1,18 +1,33 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, 
-    net::TcpListener
+    net::TcpListener, sync::broadcast
 };
+
+
+// turbofish example: 
+fn give_me_default<T>() -> T where T: Default,{
+    Default::default()
+}
+
 
 // future is a thing with no known value at the moment
 // Rust doesn't do async by default
 #[tokio::main]
 async fn main() {
+    let _value = give_me_default::<i32>();
+    
     // tcp echo server
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, _rx) = broadcast::channel(10);
+
+
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
-        
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
         tokio::spawn( async move {
 
             let (reader, mut writer) = socket.split();
@@ -22,14 +37,25 @@ async fn main() {
             let mut line = String::new();
     
             loop {
-                    
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-    
-                if bytes_read == 0 {
-                    break;
+                // useful when things need to use shared state with a finite number of things. 
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+
                 }
-                writer.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
+                
     
             }
         });
